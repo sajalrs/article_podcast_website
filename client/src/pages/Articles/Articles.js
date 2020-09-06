73cce4d7b8dc24495c159d70beb210eddea49ad6
sajalrs <sajalsatyal@gmail.com>
2020-09-06 18:26:37 -0500

    ok sidebar now works more conventionally

--- client/src/pages/Articles/Articles.js
+++ client/src/pages/Articles/Articles.js
@@ -1,25 +1,24 @@
-import React,{useRef, useState, useEffect} from "react";
+import React, { useRef, useState, useEffect } from "react";
 import MediumCard from "../../components/Cards/MediumCard/MediumCard.js";
 import LargeCard from "../../components/Cards/LargeCard/LargeCard.js";
 import SidePanel from "../../components/SidePanel/SidePanel.js";
 import Card from "../../components/Cards/Card.js";
 import styles from "./Articles.module.css";
-import Header from "../../components/Header/Header"
-import Footer from "../../components/Footer/Footer"
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 import SmallCard from "../../components/Cards/SmallCard/SmallCard.js";
-import {useHistory} from 'react-router-dom'
+import { useHistory } from "react-router-dom";
 
 const Articles = (props) => {
   const history = useHistory();
   const [sidePanelFixed, setSidePanelFix] = useState(false);
-  const headerBoxRef = useRef()
+  const headerBoxRef = useRef();
   useEffect(() => {
-    window.scrollTo(0,0);
-  },[])
+    window.scrollTo(0, 0);
+  }, []);
   useEffect(() => {
     const fixNavbar = (e) => {
-      if (window.scrollY >  (headerBoxRef.current.clientHeight-66)) {
-       
+      if (window.scrollY > headerBoxRef.current.clientHeight - 66) {
         setSidePanelFix(true);
       } else {
         setSidePanelFix(false);
@@ -30,12 +29,11 @@ const Articles = (props) => {
     return () => {
       window.removeEventListener("scroll", fixNavbar);
     };
-  },[sidePanelFixed]);
+  }, [sidePanelFixed]);
   const [homepageLinks, setHomePageLinks] = useState({
-    links: []
-  })
+    links: [],
+  });
   useEffect(() => {
-
     const getHomepageLinks = async () => {
       const response = await fetch("articles/pages");
       const body = await response.json();
@@ -44,77 +42,82 @@ const Articles = (props) => {
       return body;
     };
 
-
     getHomepageLinks().then((res) => {
-      setHomePageLinks({links: res["links"]})
+      setHomePageLinks({ links: res["links"] });
     });
-    }, []);
+  }, []);
 
-    const getLinkFunction = (linkType) => {
-      switch (linkType) {
-        case Card.LinkType["video-youtube"]:
-          return props.playVideo;
-        case Card.LinkType["article-internal"]:
-          return ((articleLink) => {
-            history.push(articleLink);
-          });
-        default:
-          break;
-      }
-    };
+  const getLinkFunction = (linkType) => {
+    switch (linkType) {
+      case Card.LinkType["video-youtube"]:
+        return props.playVideo;
+      case Card.LinkType["article-internal"]:
+        return (articleLink) => {
+          history.push(articleLink);
+        };
+      default:
+        break;
+    }
+  };
 
-
-  const renderOnceSidePanel =  (<SidePanel
-  sideBarClicked={props.sideBarClicked}
-  setSideBarClicked={props.setSideBarClicked}
-  youtubeIds={props.youtubeIds}
-  getImageLink={props.getImageLink}
-  playVideo={props.playVideo}
-  getHyperLink={props.getHyperLink}
- />)
+  const renderOnceSidePanel = (
+    <SidePanel
+      sideBarClicked={props.sideBarClicked}
+      setSideBarClicked={props.setSideBarClicked}
+      youtubeIds={props.youtubeIds}
+      getImageLink={props.getImageLink}
+      playVideo={props.playVideo}
+      getHyperLink={props.getHyperLink}
+      sidePanelFixed={sidePanelFixed}
+    />
+  );
   return (
-    
     <div className={styles["Articles"]}>
-    <Header ref={headerBoxRef} className={styles["Header"]} navbarClicked={props.navbarClicked} setNavbarClicked={props.setNavbarClicked} sideBarClicked={props.sideBarClicked} setSideBarClicked={props.setSideBarClicked}/>
+      <Header
+        ref={headerBoxRef}
+        className={styles["Header"]}
+        navbarClicked={props.navbarClicked}
+        setNavbarClicked={props.setNavbarClicked}
+        sideBarClicked={props.sideBarClicked}
+        setSideBarClicked={props.setSideBarClicked}
+      />
       <div className={styles["content-pane"]}>
         <div className={styles["main-pane"]}>
-            {homepageLinks.links.map((item) => {
-                 return (<div className={`${styles["main-pane-item"]}`}>
-                    <LargeCard
-                      image={item.image}
-                      date={item.date}
-                      title={item.title}
-                      text={item.description}
-                      author={item.author}
-                      LinkType={item.LinkType}
-                      link={props.getHyperLink(item.LinkType)(item.to)}
-                      onClick={getLinkFunction(item.LinkType)}/>
-                 </div>)
-            })}
-         
-     
+          {homepageLinks.links.map((item) => {
+            return (
+              <div className={`${styles["main-pane-item"]}`}>
+                <LargeCard
+                  image={item.image}
+                  date={item.date}
+                  title={item.title}
+                  text={item.description}
+                  author={item.author}
+                  LinkType={item.LinkType}
+                  link={props.getHyperLink(item.LinkType)(item.to)}
+                  onClick={getLinkFunction(item.LinkType)}
+                />
+              </div>
+            );
+          })}
         </div>
-        <div className={styles["side-pane"]}>
-         
-        </div>
+        <div className={styles["side-pane"]}></div>
         {sidePanelFixed ? (
-          <div style={{ position: "fixed", top: (props.topOffset), right: "0px"}}>
+          <div
+            style={{ position: "fixed", top: props.topOffset, right: "0px" }}
+          >
             {renderOnceSidePanel}
           </div>
-        
-      ) : (
-        <div style={{marginTop: (props.topOffset)}}>
-          {renderOnceSidePanel}
-        </div>
-      )}
-       
+        ) : (
+          <div style={{ marginTop: props.topOffset }}>
+            {renderOnceSidePanel}
+          </div>
+        )}
       </div>
-    
-      <div className={styles["footer-container"]}> 
-          <Footer />
-        </div>
+
+      <div className={styles["footer-container"]}>
+        <Footer />
+      </div>
     </div>
-    
   );
 };
 
