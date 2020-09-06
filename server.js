89ce3535843c7a18a8a10d41c874363d374ad311
sajalrs <sajalsatyal@gmail.com>
2020-09-05 21:53:52 -0500

    added path to server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const articlesRoute = require('./routes/articles');
+const path = require('path');
 require('dotenv/config');
 app.use(express.urlencoded({
   extended: true
