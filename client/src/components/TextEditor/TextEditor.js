7bcab4d741420b33bc7352cb9c0a4aac3799f9b5
sajalrs <sajalsatyal@gmail.com>
2020-09-08 15:51:47 -0500

    Image addition works with text

--- client/src/components/TextEditor/TextEditor.js
+++ client/src/components/TextEditor/TextEditor.js
@@ -1,205 +1,253 @@
-import React, { useCallback, useMemo, useState } from 'react'
-import isHotkey from 'is-hotkey'
-import { Editable, withReact, useSlate, Slate } from 'slate-react'
-import { Editor, Transforms, createEditor, Node } from 'slate'
-import { withHistory } from 'slate-history'
-
+import React, { useCallback, useMemo, useState } from "react";
+import isHotkey from "is-hotkey";
+import { Editable, withReact, useSlate, Slate } from "slate-react";
+import { Editor, Transforms, createEditor, Node } from "slate";
+import { withHistory } from "slate-history";
+import isUrl from "is-url";
+import imageExtensions from "image-extensions";
 const HOTKEYS = {
-  'mod+b': 'bold',
-  'mod+i': 'italic',
-  'mod+u': 'underline',
-  'mod+`': 'code',
-}
+  "mod+b": "bold",
+  "mod+i": "italic",
+  "mod+u": "underline",
+  "mod+`": "code",
+};
 
-const LIST_TYPES = ['numbered-list', 'bulleted-list']
+const LIST_TYPES = ["numbered-list", "bulleted-list"];
 
 const TextEditor = () => {
-  const [value, setValue] = useState(initialValue)
-  const renderElement = useCallback(props => <Element {...props} />, [])
-  const renderLeaf = useCallback(props => <Leaf {...props} />, [])
-  const editor = useMemo(() => withHistory(withReact(createEditor())), [])
+  const [value, setValue] = useState(initialValue);
+  const renderElement = useCallback((props) => <Element {...props} />, []);
+  const renderLeaf = useCallback((props) => <Leaf {...props} />, []);
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
 
   return (
-    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+    <Slate editor={editor} value={value} onChange={(value) => setValue(value)}>
       <div>
-      <MarkButton format="bold" />
-        <MarkButton format="italic"/>
-        <MarkButton format="underline"  />
+        <MarkButton format="bold" />
+        <MarkButton format="italic" />
+        <MarkButton format="underline" />
         <MarkButton format="code" />
-        <BlockButton format="heading-one"  />
-        <BlockButton format="heading-two"/>
+        <BlockButton format="heading-one" />
+        <BlockButton format="heading-two" />
         <BlockButton format="block-quote" />
         <BlockButton format="numbered-list" />
-        <BlockButton format="bulleted-list"  />
-      
+        <BlockButton format="bulleted-list" />
+        <BlockButton format="image" />
       </div>
-      
+
       <Editable
         renderElement={renderElement}
         renderLeaf={renderLeaf}
         placeholder="Enter some rich text…"
         spellCheck
         autoFocus
-        onKeyDown={event => {
+        onKeyDown={(event) => {
           for (const hotkey in HOTKEYS) {
             if (isHotkey(hotkey, event)) {
-              event.preventDefault()
-              const mark = HOTKEYS[hotkey]
-              toggleMark(editor, mark)
+              event.preventDefault();
+              const mark = HOTKEYS[hotkey];
+              toggleMark(editor, mark);
             }
           }
         }}
       />
     </Slate>
-  )
-}
+  );
+};
 
 const toggleBlock = (editor, format) => {
-  const isActive = isBlockActive(editor, format)
-  const isList = LIST_TYPES.includes(format)
-
+  const isActive = isBlockActive(editor, format);
+  const isList = LIST_TYPES.includes(format);
+  const isImage = format === "image";
   Transforms.unwrapNodes(editor, {
-    match: n => LIST_TYPES.includes(n.type),
+    match: (n) => LIST_TYPES.includes(n.type),
     split: true,
-  })
+  });
 
   Transforms.setNodes(editor, {
-    type: isActive ? 'paragraph' : isList ? 'list-item' : format,
-  })
+    type: isActive ? "paragraph" : isList ? "list-item" : !isImage? format : "",
+  });
 
   if (!isActive && isList) {
-    const block = { type: format, children: [] }
-    Transforms.wrapNodes(editor, block)
+    const block = { type: format, children: [] };
+    Transforms.wrapNodes(editor, block);
   }
-}
+};
 
 const toggleMark = (editor, format) => {
-  const isActive = isMarkActive(editor, format)
+  const isActive = isMarkActive(editor, format);
 
   if (isActive) {
-    Editor.removeMark(editor, format)
+    Editor.removeMark(editor, format);
   } else {
-    Editor.addMark(editor, format, true)
+    Editor.addMark(editor, format, true);
   }
-}
+};
 
 const isBlockActive = (editor, format) => {
   const [match] = Editor.nodes(editor, {
-    match: n => n.type === format,
-  })
-
-  return !!match
-}
+    match: (n) => n.type === format,
+  });
+  
+    return !!match;
+  
+ 
+};
 
 const isMarkActive = (editor, format) => {
-  const marks = Editor.marks(editor)
-  return marks ? marks[format] === true : false
-}
+  const marks = Editor.marks(editor);
+  return marks ? marks[format] === true : false;
+};
 
 const Element = ({ attributes, children, element }) => {
   switch (element.type) {
-    case 'block-quote':
-      return <blockquote {...attributes}>{children}</blockquote>
-    case 'bulleted-list':
-      return <ul {...attributes}>{children}</ul>
-    case 'heading-one':
-      return <h1 {...attributes}>{children}</h1>
-    case 'heading-two':
-      return <h2 {...attributes}>{children}</h2>
-    case 'list-item':
-      return <li {...attributes}>{children}</li>
-    case 'numbered-list':
-      return <ol {...attributes}>{children}</ol>
+    case "block-quote":
+      return <blockquote {...attributes}>{children}</blockquote>;
+    case "bulleted-list":
+      return <ul {...attributes}>{children}</ul>;
+    case "heading-one":
+      return <h1 {...attributes}>{children}</h1>;
+    case "heading-two":
+      return <h2 {...attributes}>{children}</h2>;
+    case "list-item":
+      return <li {...attributes}>{children}</li>;
+    case "numbered-list":
+      return <ol {...attributes}>{children}</ol>;
+    case "image":
+      return (
+        <div {...attributes}>
+          <div contentEditable={false}>
+            <img
+              src={element.url}
+            />
+          </div>
+          {children}
+        </div>
+      );
     default:
-      return <p {...attributes}>{children}</p>
+      return <p {...attributes}>{children}</p>;
   }
-}
+};
 
 const Leaf = ({ attributes, children, leaf }) => {
   if (leaf.bold) {
-    children = <strong>{children}</strong>
+    children = <strong>{children}</strong>;
   }
 
   if (leaf.code) {
-    children = <code>{children}</code>
+    children = <code>{children}</code>;
   }
 
   if (leaf.italic) {
-    children = <em>{children}</em>
+    children = <em>{children}</em>;
   }
 
   if (leaf.underline) {
-    children = <u>{children}</u>
+    children = <u>{children}</u>;
   }
 
-  return <span {...attributes}>{children}</span>
-}
+  return <span {...attributes}>{children}</span>;
+};
 
-const BlockButton = ({ format}) => {
-  const editor = useSlate()
+const BlockButton = ({ format }) => {
+  const editor = useSlate();
+  if (format === "image") {
+    return (<button
+      active={isBlockActive(editor, format)}
+      onMouseDown={(event) => {
+        event.preventDefault();
+        const url = window.prompt('Enter the URL of the image:')
+        if(!url) return 
+        toggleBlock(editor, format);
+        insertImage(editor, url);
+      }}
+    >
+      {format}
+    </button>);
+  } else {
+    return (
+      <button
+        active={isBlockActive(editor, format)}
+        onMouseDown={(event) => {
+          event.preventDefault();
+          toggleBlock(editor, format);
+        }}
+      >
+        {format}
+      </button>
+    );
+  }
+};
+
+const MarkButton = ({ format }) => {
+  const editor = useSlate();
   return (
     <button
-      active={isBlockActive(editor, format)}
-      onMouseDown={event => {
-        event.preventDefault()
-        toggleBlock(editor, format)
+      active={isMarkActive(editor, format)}
+      onMouseDown={(event) => {
+        event.preventDefault();
+        toggleMark(editor, format);
       }}
     >
       {format}
     </button>
-  )
-}
+  );
+};
 
-const MarkButton = ({ format}) => {
-  const editor = useSlate()
-  return (
-    <button
-      active={isMarkActive(editor, format)}
-      onMouseDown={event => {
-        event.preventDefault()
-        toggleMark(editor, format)
-      }}
-    >
-        {format}
-    </button>
-  )
-}
+const insertImage = (editor, url) => {
+  const text = { text: "" };
+  const image = { type: "image", url, children: [text] };
+  Transforms.insertNodes(editor, image);
+};
+
+const isImageUrl = (url) => {
+  if (!url) return false;
+  if (!isUrl(url)) return false;
+  const ext = new URL(url).pathname.split(".").pop();
+  return imageExtensions.includes(ext);
+};
+
 
 const initialValue = [
   {
-    type: 'paragraph',
+    type: "paragraph",
     children: [
-      { text: 'This is editable ' },
-      { text: 'rich', bold: true },
-      { text: ' text, ' },
-      { text: 'much', italic: true },
-      { text: ' better than a ' },
-      { text: '<textarea>', code: true },
-      { text: '!' },
+      { text: "This is editable " },
+      { text: "rich", bold: true },
+      { text: " text, " },
+      { text: "much", italic: true },
+      { text: " better than a " },
+      { text: "<textarea>", code: true },
+      { text: "!" },
     ],
   },
   {
-    type: 'paragraph',
+    type: "paragraph",
     children: [
       {
         text:
           "Since it's rich text, you can do things like turn a selection of text ",
       },
-      { text: 'bold', bold: true },
+      { text: "bold", bold: true },
       {
         text:
-          ', or add a semantically rendered block quote in the middle of the page, like this:',
+          ", or add a semantically rendered block quote in the middle of the page, like this:",
       },
     ],
   },
   {
-    type: 'block-quote',
-    children: [{ text: 'A wise quote.' }],
+    type: "block-quote",
+    children: [{ text: "A wise quote." }],
   },
   {
-    type: 'paragraph',
-    children: [{ text: 'Try it out for yourself!' }],
+    type: "paragraph",
+    children: [{ text: "Try it out for yourself!" }],
   },
-]
+  {
+    type: 'image',
+    url: 'https://source.unsplash.com/kFrdX5IeQzI',
+    children: [{ text: '' }],
+  },
+];
 
-export default TextEditor
+export default TextEditor;
---
+++ client/src/pages/Edit.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import TextEditor from '../components/TextEditor/TextEditor.js'
+
+const Edit = () => {
+    return (
+    <TextEditor/>
+        
+    )
+}
+
+export default Edit;
