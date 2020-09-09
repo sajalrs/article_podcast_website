604d0e334efc62bb1cb792d41c985d5037e3e682
sajalrs <sajalsatyal@gmail.com>
2020-09-09 15:14:53 -0500

    after a lot of fighting slate works on android

---
+++ client/src/components/TextEditor/TextEditorQuill.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const TextEditor = () => {
+  const [value, setValue] = useState("");
+
+  const modules = {
+    toolbar: {
+      container: [
+        [{ header: "1" }, { header: "2" }, { font: [] }],
+        [{ size: [] }],
+        ["bold", "italic", "underline", "strike", "blockquote"],
+        [
+          { list: "ordered" },
+          { list: "bullet" },
+          { indent: "-1" },
+          { indent: "+1" },
+        ],
+        ["link", "image", "video"],
+        ["clean"],
+      ],
+    },
+    clipboard: {
+      // toggle to add extra line breaks when pasting HTML:
+      matchVisual: false,
+    },
+  };
+
+  const formats = [
+    'header', 'font', 'size',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image', 'video'
+  ]
+
+  return <ReactQuill formats={formats} modules={modules} theme="snow" value={value} onChange={setValue} />;
+};
+
+export default TextEditor;
