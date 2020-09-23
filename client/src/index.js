4802fb50ade24fa2ef4b6ecdf5f1d8f62c407f06
sajalrs <sajalsatyal@gmail.com>
2020-09-22 21:14:11 -0500

    hooked up redux provider

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
 import rootReducer from './reducers'
-
+import {Provider} from 'react-redux'
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
+
   document.getElementById('root')
 );
 
