52029ff9378868949a4c0a259e21df4d41f11ab2
sajalrs <sajalsatyal@gmail.com>
2020-09-22 21:53:51 -0500

    sidebar clicked truth source from redux store and not props

--- client/src/components/SidePanel/SidePanel.js
+++ client/src/components/SidePanel/SidePanel.js
@@ -3,18 +3,23 @@ import SmallCard from "../Cards/SmallCard/SmallCard.js";
 import styles from "./SidePanel.module.css";
 import Card from "../Cards/Card.js";
 import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
+import {useSelector, useDispatch} from 'react-redux'
+import {setSidebarClicked} from '../.././actions';
+
 
 const SidePanel = (props) => {
   const cardListRef = useRef();
   const sidePanelDivRef = useRef();
   const [titlePos, setTitlePos] = useState();
+  const sidebarClicked = useSelector(state => state.sidebarClicked);
+  const dispatch = useDispatch();
   useEffect(() => {
-    if(props.sideBarClicked && props.sidePanelFixed){
+    if(sidebarClicked && props.sidePanelFixed){
       disableBodyScroll(cardListRef.current);
     } else{
       enableBodyScroll(cardListRef.current);
     }
-  }, [props.sideBarClicked, props.sidePanelFixed])
+  }, [sidebarClicked, props.sidePanelFixed])
   
   useEffect(() => {
     setTitlePos(sidePanelDivRef.current.innerWidth);
@@ -25,16 +30,16 @@ const SidePanel = (props) => {
       ref={sidePanelDivRef}
       style={{top: props.top}}
       className={
-        props.sideBarClicked
+        sidebarClicked
           ? `${styles["SidePanel"]} ${styles["maximized"]}`
           : `${styles["SidePanel"]} ${styles["minimized"]}`
       }
     >
       {/* style={{right: titlePos}} */}
-      <label className={styles["side-panel-title"]}  onClick={() => {props.setSideBarClicked(!props.sideBarClicked)}}>
+      <label className={styles["side-panel-title"]}  onClick={() => {dispatch(setSidebarClicked(!sidebarClicked))}}>
         <label className={styles["side-panel-title-text"]}>VIDEO PODCASTS{" "}</label>
         <i
-          className={props.sideBarClicked? `${styles["fas"]} ${styles["fa-chevron-up"]} fas fa-chevron-up` :`${styles["fas"]} ${styles["fa-chevron-down"]} fas fa-chevron-down`}
+          className={sidebarClicked? `${styles["fas"]} ${styles["fa-chevron-up"]} fas fa-chevron-up` :`${styles["fas"]} ${styles["fa-chevron-down"]} fas fa-chevron-down`}
         ></i>
       </label>
       <ul ref={cardListRef} className={props.sidePanelFixed? `${styles["card-list"]} ${styles["card-list-fixed"]} `: `${styles["card-list"]} ${styles["card-list-not-fixed"]} ` } >
