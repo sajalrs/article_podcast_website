382bdb1deba7813db5bdc2f197a15738b6fd593c
sajalrs <sajalsatyal@gmail.com>
2020-09-23 14:54:39 -0500

    added reducer for audioplayer and also added relevant actions

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import navbarReducer from './navbarReducer';
 import sidebarReducer from './sidebarReducer';
+import audioPlayerReducer from './audioPlayerReducer'
 import {combineReducers} from 'redux';
 
 const rootReducer = combineReducers({
   navbar: navbarReducer,
-  sidebar: sidebarReducer
+  sidebar: sidebarReducer,
+  audioPlayer: audioPlayerReducer
 })
 
 export default rootReducer;
