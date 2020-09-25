948a79d0610fd9f86e4085162238d3849fa62121
sajalrs <sajalsatyal@gmail.com>
2020-09-24 21:13:05 -0500

    videoplayer and audioplayer analogous

--- client/src/redux/reducers/videoPlayerReducer.js
+++ client/src/redux/reducers/videoPlayerReducer.js
@@ -1,22 +1,31 @@
 const videoPlayerReducer = (
-    state = {
-        selected: "https://www.youtube.com/embed/jbG9LJs_Npg?rel=0&autoplay=1",
-        isPlaying: false,
-        youtubeVideos: []
-    },
-    actions
-  ) => {
-    switch (actions.type) {
-      case "SET_VIDEOPLAYER_SELECTED":
-        return { ...state, selected: actions.payload };
-      case "SET_VIDEOPLAYER_IS_PLAYING":
-        return { ...state, isPlaying: actions.payload };
-      case "SET_VIDEOPLAYER_YOUTUBE_VIDEOS":
-          return {...state, youtubeVideos: actions.payload};
-      default:
-        return state;
-    }
-  };
-  
-  export default videoPlayerReducer;
-  
+  state = {
+    selected: 0,
+    isPlaying: false,
+    youtubeVideos: [
+      {
+        id: "vpWIvnnWxaY",
+        title:
+          "The False 9 Podcast Live Premier League and Transfer Window Preview",
+        image: "https://img.youtube.com/vi/vpWIvnnWxaY/hqdefault.jpg",
+        link:
+          "https://www.youtube.com/embed/vpWIvnnWxaY?rel=0&start=0&autoplay=1",
+        date: "2020-09-11T16:56:50Z",
+      },
+    ],
+  },
+  actions
+) => {
+  switch (actions.type) {
+    case "SET_VIDEOPLAYER_SELECTED":
+      return { ...state, selected: actions.payload };
+    case "SET_VIDEOPLAYER_IS_PLAYING":
+      return { ...state, isPlaying: actions.payload };
+    case "SET_VIDEOPLAYER_YOUTUBE_VIDEOS":
+      return { ...state, youtubeVideos: actions.payload };
+    default:
+      return state;
+  }
+};
+
+export default videoPlayerReducer;
