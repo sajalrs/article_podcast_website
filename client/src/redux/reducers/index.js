c6e97c05c1074f8abd260fa969e37670dc6c03e7
sajalrs <sajalsatyal@gmail.com>
2020-09-24 20:35:15 -0500

    video ids downloaded and stored in videoplayer global state

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,12 +1,15 @@
 import navbarReducer from './navbarReducer';
 import sidebarReducer from './sidebarReducer';
 import audioPlayerReducer from './audioPlayerReducer'
+import videoPlayerReducer from './videoPlayerReducer'
 import {combineReducers} from 'redux';
+import VideoPlayer from '../../components/VideoPlayer/VideoPlayer';
 
 const rootReducer = combineReducers({
   navbar: navbarReducer,
   sidebar: sidebarReducer,
-  audioPlayer: audioPlayerReducer
+  audioPlayer: audioPlayerReducer,
+  videoPlayer: videoPlayerReducer
 })
 
 export default rootReducer;
