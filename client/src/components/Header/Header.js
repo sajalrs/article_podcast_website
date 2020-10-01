41b2d2a92f72c70fc0733a6e417532ae23d9af72
sajalrs <sajalsatyal@gmail.com>
2020-10-01 13:28:29 -0500

    decoupled navbar from header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,45 +5,12 @@ import FalseNineIcon from '../../Icons/FalseNineFitting'
 import FIcon from '../../Icons/F'
 import NineIcon from '../../Icons/Nine'
 const Header = forwardRef((props, ref) => {
-  const [navFixed, changeNavFix] = useState(false);
-  const headerBoxRef = useRef();
-  useEffect(() => {
-    const fixNavbar = (e) => {
-      if (window.scrollY > headerBoxRef.current.clientHeight) {
-        changeNavFix(true);
-      } else {
-        changeNavFix(false);
-      }
-    };
-
-    window.addEventListener("scroll", fixNavbar);
-    return () => {
-      window.removeEventListener("scroll", fixNavbar);
-    };
-  });
-
-  const renderOnceNavbar = (
-    <Navbar/>
-  );
 
   return (
     <header ref={ref}>
-      <div className={styles["header"]} ref={headerBoxRef}>
+      <div className={styles["header"]}>
         <h1 id={styles["header-title"]}><span className={styles["title-word"]}>The</span><span className={styles["title-word"]}> <FIcon height="9vw" width="7vw"/>alse</span><span className={styles["title-word"]}><NineIcon  height="9vw" width="8vw"/></span></h1>
       </div>
-
-      {navFixed ? (
-        <div>
-          <div style={{ width: "100%", height: "66px" }}></div>
-          <div style={{ position: "fixed", width: "100%", top: "0px", zIndex: 1 }}>
-            {renderOnceNavbar}
-          </div>
-        </div>
-      ) : (
-        <div style={{ position: "relative", width: "100%" }}>
-          {renderOnceNavbar}
-        </div>
-      )}
     </header>
   );
 });
