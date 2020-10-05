cb7a585a54dadc1c545c22280a69aa5876d650c9
sajalrs <sajalsatyal@gmail.com>
2020-10-05 12:24:19 -0500

    pages architecture works

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -7,46 +7,35 @@ import {useSelector, useDispatch} from 'react-redux'
 import {setNavbarClicked, setSidebarClicked, setTopOffset} from '../../redux/actions';
 
 const Navbar = (props) => {
-    const navbarClicked = useSelector(state => state.navbar.clicked)
-    const sidebarClicked = useSelector(state => state.sidebar.clicked)
-    const topOffset = useSelector(state => state.sidebar.topOffset)
-    const dispatch = useDispatch();
+    // const navbarClicked = useSelector(state => state.navbar.clicked)
+    // const sidebarClicked = useSelector(state => state.sidebar.clicked)
+    // const topOffset = useSelector(state => state.sidebar.topOffset)
+    // const dispatch = useDispatch();
    
-    useEffect(() => {
-    
-            if (navbarClicked) {
-                dispatch(setTopOffset(topOffset + 140));
-              dispatch(setSidebarClicked(false));
-            } else {
-              if (topOffset != 100) {
-                dispatch(setTopOffset(topOffset - 140));
-              }
-            }
-   
-    }, [navbarClicked])
+
 
     useEffect(() => {
         const updateDropDown = () => {
-          if (window.innerWidth > 1250 && navbarClicked) {
-            dispatch(setNavbarClicked(window.innerWidth < 1250));
+          if (window.innerWidth > 1250 && props.navbarClicked) {
+            props.setNavbarClicked(window.innerWidth < 1250);
           }
         };
         window.addEventListener("resize", updateDropDown);
         return () => {
           window.addEventListener("resize", updateDropDown);
         };
-      }, [navbarClicked]);
+      }, [props.navbarClicked]);
 
     return(
         <div className={styles["Navbar"]}>
             <div className={styles["navbar-title-logo"]}><FalseNineIcon width="37px" height="37px"/><label className={styles["navbar-title"]}>THE FALSE 9</label></div>
             <nav>
                 <div className={styles["navigation"]}>
-                <ul className={navbarClicked?`${styles["nav-links-list"]} ${styles["nav-links-list-active"]}`:`${styles["nav-links-list"]} ${styles["nav-links-list-inactive"]}`}>
-    {MenuItems.map((item, index) => {return <li key={index} className={index%2===0?`${styles["list-item"]} ${styles["list-item-even"]}`:`${styles["list-item"]} ${styles["list-item-odd"]}`}><Link onClick={() => {if(sidebarClicked){dispatch(setSidebarClicked(false))};dispatch(setNavbarClicked(false));}} to={item.url} className={styles[item.className]}><label className={styles["link-text"]}>{item.title.toUpperCase()}</label></Link></li>;})}
+                <ul className={props.navbarClicked?`${styles["nav-links-list"]} ${styles["nav-links-list-active"]}`:`${styles["nav-links-list"]} ${styles["nav-links-list-inactive"]}`}>
+    {MenuItems.map((item, index) => {return <li key={index} className={index%2===0?`${styles["list-item"]} ${styles["list-item-even"]}`:`${styles["list-item"]} ${styles["list-item-odd"]}`}><Link onClick={() => {if(props.sidebarClicked){props.setSidebarClicked(false)};props.setNavbarClicked(false);}} to={item.url} className={styles[item.className]}><label className={styles["link-text"]}>{item.title.toUpperCase()}</label></Link></li>;})}
                 </ul>
                 <ul className={styles["social-links"]}>
-                    <li><i className={navbarClicked?`${styles["fas"]} ${styles["fa-times"]} fas fa-times`:`${styles["fas"]} ${styles["fa-bars"]} fas fa-bars`} onClick={()=> {dispatch(setNavbarClicked(!navbarClicked));}}/></li>
+                    <li><i className={props.navbarClicked?`${styles["fas"]} ${styles["fa-times"]} fas fa-times`:`${styles["fas"]} ${styles["fa-bars"]} fas fa-bars`} onClick={()=> {props.setNavbarClicked(!props.navbarClicked);}}/></li>
                     <li><a href="https://www.facebook.com/False9Podcast" target="_blank"><i className={`${styles["fab"]} ${styles["fa-facebook-f"]} fab fa-facebook-f`}/></a></li>
                     <li><a href="https://twitter.com/TFalse9podcast" target="_blank"><i className={`${styles["fab"]} ${styles["fa-twitter"]} fab fa-twitter`}/></a></li>
                     <li><a href="https://www.youtube.com/channel/UCV2Y62okiOmoXzYIh_xAcGw" target="_blank"><i className={`${styles["fab"]} ${styles["fa-youtube"]} fab fa-youtube`}/></a></li>
