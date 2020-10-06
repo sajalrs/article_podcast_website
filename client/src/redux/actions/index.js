cc837521d16e3b5c7ec87b23f665fa343f25ab0b
sajalrs <sajalsatyal@gmail.com>
2020-10-06 14:56:34 -0500

    removed reducers and actions for sidebar and navbar since click and docking/undocking handled by props

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,35 +1,3 @@
-//Navbar Actions
-export const setNavbarClicked = (setTo) => {
-    return{
-        type: 'SET_NAVBAR_CLICKED',
-        payload: setTo
-    }
-}
-
-
-//Sidebar Actions
-export const setSidebarClicked = (setTo) => {
-    return{
-        type: 'SET_SIDEBAR_CLICKED',
-        payload: setTo
-    }
-}
-
-
-export const setSidebarFixed = (setTo) => {
-    return{
-        type: 'SET_SIDEBAR_FIXED',
-        payload: setTo
-    }
-}
-
-export const setTopOffset = (setTo) => {
-    return{
-        type: 'SET_TOP_OFFSET',
-        payload: setTo
-    }
-}
-
 //AudioPlayer Actions
 export const setAudioPlayerIsPlaying = (setTo) => {
     return {
