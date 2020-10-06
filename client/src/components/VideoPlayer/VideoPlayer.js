69dd372e01cfbea82b587b5ac72d0033b58695d9
sajalrs <sajalsatyal@gmail.com>
2020-10-06 14:58:44 -0500

    videoplayer works

--- client/src/components/VideoPlayer/VideoPlayer.js
+++ client/src/components/VideoPlayer/VideoPlayer.js
@@ -4,7 +4,8 @@ import {
   disableBodyScroll,
   enableBodyScroll,
 } from "body-scroll-lock";
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
+import {closeVideo} from '../../redux/actions'
 const VideoPlayer = (props) => {
     
     const isPlaying = useSelector(state => state.videoPlayer.isPlaying)
@@ -20,13 +21,14 @@ const VideoPlayer = (props) => {
     },[isPlaying, props.scrollLockRef.current])
 
     const selected = useSelector(state=> state.videoPlayer.selected)
+    const dispatch = useDispatch();
     return (
         isPlaying? 
             (<div className={styles["youtube-video-container"]} id="youtube-video-container">
             
               <div className={styles["closeable"]}>
                 <div className={styles["close-button"]}>
-                  <i className={`${styles["fas"]} ${styles["fa-times"]} fas fa-times`} onClick={() => {props.closeVideo()}}></i>
+                  <i className={`${styles["fas"]} ${styles["fa-times"]} fas fa-times`} onClick={() => {dispatch(closeVideo())}}></i>
                   <label className={styles["close-btn-label"]}>Close</label>
                 </div>
                 <div className={styles["video"]}>
