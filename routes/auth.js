d4407b6f2f7712c838626be450dc2a4309bb4fbe
sajalrs <sajalsatyal@gmail.com>
2020-10-09 16:01:43 -0500

    added verification middleware

--- routes/auth.js
+++ routes/auth.js
@@ -55,8 +55,8 @@ router.post('/login', async (req, res) => {
   if(!validPass) return res.status(400).send({error: "The email or password is incorrect" })
 
   const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+  res.header('auth-token', token).send(token)
 
-  res.send("Logged In")
 })
 
 module.exports = router;
