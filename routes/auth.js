c9e2a2f573029d8d89f387931a095b3af577a59d
sajalrs <sajalsatyal@gmail.com>
2020-10-09 15:49:22 -0500

    login works

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/Users");
-const { registerValidation } = require("../validation/validation");
+const { registerValidation, loginValidation } = require("../validation/validation");
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 router.post("/register", async (req, res) => {
   const { error } = registerValidation(req.body);
@@ -30,10 +31,30 @@ router.post("/register", async (req, res) => {
 
   try {
     const savedUser = await user.save();
-    res.send({ user: savedUser });
+    res.send({ user: user._id });
   } catch (err) {
     res.status(400).send(err);
   }
 });
+
+router.post('/login', async (req, res) => {
+  const { error } = loginValidation(req.body);
+  if (error) {
+    const toReturn = error.details[0].message
+      .replace('"email"', "Email")
+      .replace('"password"', "Password");
+    return res.status(400).send({ error: toReturn });
+  }
+
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user)
+    return res.status(400).send({ error: "The email or password is incorrect" });
+
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if(!validPass) return res.status(400).send({error: "The email or password is incorrect" })
+
+  res.send("Logged In")
+})
 
 module.exports = router;
