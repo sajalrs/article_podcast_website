494253564268f4c4f279e1768169999b5f5e1a39
sajalrs <sajalsatyal@gmail.com>
2020-10-09 14:33:47 -0500

    added auth route and joi validation to auth routhe

--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,33 @@
-const express = require('express')
-const router = express.Router()
-const Users = require("../models/Users")
+const express = require("express");
+const router = express.Router();
+const User = require("../models/Users");
+const Joi = require("@hapi/joi");
+const schema = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
+});
 
-router.post('/', (req, res) => {
-    const user = new Users(
-        {
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        }
-    );
+router.post("/register", async (req, res) => {
 
-    user.save((err,data) => {
-        if(err){
-            res.send(err);
-        } else {
-            res.json(data);
-        }
-    });
-})
+  const {error} = schema.validate(req.body)
+  if(error){
+      const toReturn = error.details[0].message.replace('\"name\"', "Name").replace('\"email\"', "Email").replace('\"password\"', "Password")
+      return res.status(400).send({error: toReturn})
+ }
 
-module.exports = router
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  try{
+    const savedUser = await user.save();
+    res.send({user: savedUser})
+  } catch(err) {
+    res.status(400).send(err)
+  }
+});
+
+module.exports = router;
