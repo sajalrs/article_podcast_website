1cdccfb2e5d1b9e747a851ce250a0636a65b19a0
sajalrs <sajalsatyal@gmail.com>
2020-10-09 14:44:01 -0500

    separated validation concerns in its own module

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/Users");
-const { registerValidation } = require("./validation");
+const { registerValidation } = require("../validation/validation");
 
 router.post("/register", async (req, res) => {
   const { error } = registerValidation(req.body);
