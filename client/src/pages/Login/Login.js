1913e92042016f568531fdda6232a6ddec3ba8ac
sajalrs <sajalsatyal@gmail.com>
2020-10-10 17:55:39 -0500

    added login page

---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import Page from "../../components/Page/Page";
+import styles from "../../components/Page/Page.module.css";
+import {useHistory} from 'react-router-dom'
+const Login = (props) => {
+  const history = useHistory()
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+
+
+  const onEmailChange = (e) => {
+    setFormData({ ...formData, email: e.target.value });
+  };
+
+  const onPasswordChange = (e) => {
+    setFormData({ ...formData, password: e.target.value });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    registerUser()
+  }
+
+  const registerUser = async () => {
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    };
+
+    const response = await fetch(`/auth/login`, requestOptions);
+    const data = await response.json();
+ 
+    if (response.status === 400) {
+      alert(data.error);
+    } else if (response.status !== 200) {
+      throw Error(data.message);
+     } else{
+      alert("New User Registered");
+      history.push('/')
+     }
+    
+  };
+
+  const contents = (
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        className={styles["headline-form"]}
+      >
+        <div className={styles["horizontal"]}>
+          <label>Email: </label>
+          <input
+            className={styles["headline-form-input"]}
+            type="text"
+            value={formData.email}
+            onChange={onEmailChange}
+          />
+        </div>
+        <div className={styles["horizontal"]}>
+          <label>Password: </label>
+          <input
+            className={styles["headline-form-input"]}
+            type="password"
+            value={formData.password}
+            onChange={onPasswordChange}
+          />
+        </div>
+        <input
+          type="submit"
+          value="Sign Up"
+          className={styles["submit-button"]}
+        />
+      </form>
+    </div>
+  );
+
+  return <Page sidebarFixTopOffset={0} mainPane={contents} />;
+};
+
+export default Login;
