4d1efa099ea3ba8b03e81533316e91903a636653
sajalrs <sajalsatyal@gmail.com>
2020-10-10 18:26:35 -0500

    combined jwt token reducer

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,13 +2,15 @@ import audioPlayerReducer from './audioPlayerReducer'
 import videoPlayerReducer from './videoPlayerReducer'
 import blogReducer from './blogReducer'
 import headerReducer from './headerReducer'
+import jwtTokenReducer from './jwtTokenReducer'
 import {combineReducers} from 'redux';
 
 const rootReducer = combineReducers({
   audioPlayer: audioPlayerReducer,
   videoPlayer: videoPlayerReducer,
   blog: blogReducer,
-  header: headerReducer
+  header: headerReducer,
+  jwtToken: jwtTokenReducer
 })
 
 export default rootReducer;
