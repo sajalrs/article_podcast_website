dca1a9f19428e656c92093f2a1ce927e55689473
sajalrs <sajalsatyal@gmail.com>
2020-10-10 15:07:13 -0500

    added signup page

--- client/src/pages/CreateArticle.js
+++ client/src/pages/CreateArticle.js
@@ -1,23 +1,29 @@
-import React, {useEffect } from 'react'
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 const CreateArticle = () => {
-    useEffect(() => {
-      const getArticle = async () => {
-        const response = await fetch("/create/articles");
-        const body = await response.json();
-        if (response.status !== 200) throw Error(body.message);
+
+  const history = useHistory()
+  useEffect(() => {
+    const getArticle = async () => {
+      const response = await fetch("/create/articles");
+      const body = await response.json();
+      if (response.status === 401) {
+        alert(body.error);
+      } else if (response.status !== 200) {
+        throw Error(body.message);
+      }
+      return body;
+    };
+
+    getArticle().then((res) => {
   
-        return body;
-      };
+      history.push('/');
+    
+    });
+  }, []);
 
-      getArticle().then((res) => {
-        console.log(res);
-      })
-    }, [])
-
-    return(
-      <div>200</div>
-    )
-  }
+  return <div>200</div>;
+};
 
 export default CreateArticle;
