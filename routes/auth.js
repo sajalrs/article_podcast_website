426a1d638e2c3c7fc8b74cd065f96179b80e98d0
sajalrs <sajalsatyal@gmail.com>
2020-10-11 00:17:25 -0500

    token stored in cookie at endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/Users");
-const { registerValidation, loginValidation } = require("../validation/validation");
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const {
+  registerValidation,
+  loginValidation,
+} = require("../validation/validation");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { response } = require("express");
 
 router.post("/register", async (req, res) => {
@@ -27,7 +30,7 @@ router.post("/register", async (req, res) => {
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: hashPassword
+    password: hashPassword,
   });
 
   try {
@@ -38,7 +41,7 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { error } = loginValidation(req.body);
   if (error) {
     const toReturn = error.details[0].message
@@ -50,14 +53,20 @@ router.post('/login', async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
 
   if (!user)
-    return res.status(400).send({ error: "The email or password is incorrect" });
+    return res
+      .status(400)
+      .send({ error: "The email or password is incorrect" });
 
   const validPass = await bcrypt.compare(req.body.password, user.password);
-  if(!validPass) return res.status(400).send({error: "The email or password is incorrect" })
+  if (!validPass)
+    return res
+      .status(400)
+      .send({ error: "The email or password is incorrect" });
 
-  const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
   // res.header('Auth-Token', token).send({token: token})
-  res.send({token: token})
-})
+  res.cookie("token", token, { httpOnly: true }).json({token: token});
+  // res.send({ token: token });
+});
 
 module.exports = router;
