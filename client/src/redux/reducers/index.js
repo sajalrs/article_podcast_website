16ae1a815b3bf31253795a15fc2d2c24d50ab28f
sajalrs <sajalsatyal@gmail.com>
2020-10-11 23:37:12 -0500

    combined login reducer to root reducer

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,12 +2,14 @@ import audioPlayerReducer from './audioPlayerReducer'
 import videoPlayerReducer from './videoPlayerReducer'
 import blogReducer from './blogReducer'
 import headerReducer from './headerReducer'
+import loginReducer from './loginReducer'
 import {combineReducers} from 'redux';
 
 const rootReducer = combineReducers({
   audioPlayer: audioPlayerReducer,
   videoPlayer: videoPlayerReducer,
   blog: blogReducer,
+  login: loginReducer,
   header: headerReducer,
 })
 
