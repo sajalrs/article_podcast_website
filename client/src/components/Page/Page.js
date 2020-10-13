e08167f07e5d5b20556552877ec0b17ba51c20be
sajalrs <sajalsatyal@gmail.com>
2020-10-12 21:17:18 -0500

    sidebar moves even when navbar isn't fixed when navbar is expanded

--- client/src/components/Page/Page.js
+++ client/src/components/Page/Page.js
@@ -1,27 +1,32 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import SidePanel from "../../components/SidePanel/SidePanel.js";
 import styles from "./Page.module.css";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-import Navbar from "../../components/Navbar/Navbar"
-import AudioPlayer from "../../components/AudioPlayer/AudioPlayer"
-import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from "body-scroll-lock";
+import Navbar from "../../components/Navbar/Navbar";
+import AudioPlayer from "../../components/AudioPlayer/AudioPlayer";
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from "body-scroll-lock";
 
 const Page = (props) => {
- 
-  const [topOffset,setTopOffset] = useState(100)
+  const offsetInitial = useRef(50);
+  const [topOffset, setTopOffset] = useState(offsetInitial.current);
   const [navFixed, changeNavFix] = useState(false);
   const [isActive, setActive] = useState(false);
   // const [boxHeight, setBoxHeight] = useState(202);
   const audioPlayerBoxRef = useRef();
   const [audioPlayerFixed, setAudioPlayerFixed] = useState(false);
   const footerBoxRef = useRef();
-  const [sidebarClicked,setSidebarClicked] = useState(false);
+  const [sidebarClicked, setSidebarClicked] = useState(false);
   const [sidebarFixed, setSidebarFixed] = useState(false);
-  const [navbarClicked,setNavbarClicked] = useState(false);
+  const [navbarClicked, setNavbarClicked] = useState(false);
   const contentPaneBoxRef = useRef();
   const headerBoxRef = useRef();
- 
+  const sidebarFixTopOffset = useRef(props.sidebarFixTopOffset);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -35,22 +40,21 @@ const Page = (props) => {
   //   };
 
   //   const fixSidebar = (e) => {
-     
+
   //     if (window.scrollY > headerBoxRef.current.clientHeight + sidebarFixTopOffset.current && !sidebarFixed) {
   //       setSidebarFixed(true);
-        
+
   //     } else {
   //       if(sidebarFixed){
   //         setSidebarFixed(false);
 
-  //       } 
+  //       }
   //     }
   //   };
 
   //   window.addEventListener("scroll", fixSidebar);
   //   window.addEventListener("scroll", fixNavbar);
-  
-    
+
   //   return () => {
   //     window.removeEventListener("scroll", fixSidebar);
   //     window.removeEventListener("scroll", fixNavbar);
@@ -58,43 +62,47 @@ const Page = (props) => {
   //   }
   // }, []);
 
-
   useEffect(() => {
     const scrollEvents = (e) => {
-      const curScroll = window.scrollY
-      console.log(`scroll: ${curScroll} header: ${headerBoxRef.current.clientHeight} content: ${contentPaneBoxRef.current.clientHeight} footer: ${footerBoxRef.current.clientHeight}`)
-     
-      if(curScroll > headerBoxRef.current.clientHeight){
-        changeNavFix(true)
-        if(curScroll > headerBoxRef.current.clientHeight + props.sidebarFixTopOffset){
-         setSidebarFixed(true);
-          if(curScroll >  (contentPaneBoxRef.current.clientHeight - headerBoxRef.current.clientHeight - footerBoxRef.current.clientHeight)){
+      const curScroll = window.scrollY;
+      console.log(
+        `scroll: ${curScroll} header: ${headerBoxRef.current.clientHeight} content: ${contentPaneBoxRef.current.clientHeight} footer: ${footerBoxRef.current.clientHeight}`
+      );
+
+      if (curScroll > headerBoxRef.current.clientHeight) {
+        changeNavFix(true);
+        if (
+          curScroll >
+          headerBoxRef.current.clientHeight + props.sidebarFixTopOffset
+        ) {
+          setSidebarFixed(true);
+          if (
+            curScroll >
+            contentPaneBoxRef.current.clientHeight -
+              headerBoxRef.current.clientHeight -
+              footerBoxRef.current.clientHeight
+          ) {
             setAudioPlayerFixed(false);
-          } else{
+          } else {
             setAudioPlayerFixed(true);
           }
-        } else{
+        } else {
           setSidebarFixed(false);
           setAudioPlayerFixed(false);
         }
-      }else{
+      } else {
         changeNavFix(false);
         setSidebarFixed(false);
         setAudioPlayerFixed(false);
       }
-    }
+    };
 
-    window.addEventListener("scroll",scrollEvents);
-   
-  
-    
+    window.addEventListener("scroll", scrollEvents);
+
     return () => {
       window.removeEventListener("scroll", scrollEvents);
-    }
-
-  })
-
-
+    };
+  });
 
   // useEffect(() => {
   //   const body = document.body,
@@ -111,8 +119,7 @@ const Page = (props) => {
   //     setBoxHeight(Math.max(audioPlayerBoxRef.current.clientHeight, boxHeight))
   //   }
   //     const fixAudioPlayer = (e) => {
-  
-    
+
   //     if (
   //       window.scrollY >
   //       height - 2 * (footerBoxRef.current.clientHeight + boxHeight)
@@ -121,7 +128,7 @@ const Page = (props) => {
   //     } else {
   //       setAudioPlayerFixed(true);
   //     }
-    
+
   //   };
 
   //   window.addEventListener("scroll", fixAudioPlayer);
@@ -131,8 +138,6 @@ const Page = (props) => {
   // });
 
   useEffect(() => {
-
-  
     if (sidebarClicked) {
       if (navbarClicked) {
         setNavbarClicked(false);
@@ -140,38 +145,30 @@ const Page = (props) => {
     } else {
       clearAllBodyScrollLocks();
     }
-
-
-
-  }
-  , [sidebarClicked])
+  }, [sidebarClicked]);
   useEffect(() => {
-    
     if (navbarClicked) {
-        setTopOffset(topOffset + 140);
-  
-        setSidebarClicked(false);
-
+      setTopOffset(topOffset + 140);
+      sidebarFixTopOffset.current += 140;
+      setSidebarClicked(false);
     } else {
-      if (topOffset != 100) {
+      if (topOffset != offsetInitial.current) {
         setTopOffset(topOffset - 140);
-     
+        sidebarFixTopOffset.current -= 140;
       }
     }
+  }, [navbarClicked]);
 
-}, [navbarClicked])
-
- 
   // useEffect(() => {
   //   const fixSidebar = (e) => {
   //     if (window.scrollY > headerBoxRef.current.clientHeight + sidebarFixTopOffset && !sidebarFixed) {
   //       setSidebarFixed(true);
-        
+
   //     } else {
   //       if(sidebarFixed){
   //         setSidebarFixed(false);
 
-  //       } 
+  //       }
   //     }
   //   };
 
@@ -183,20 +180,21 @@ const Page = (props) => {
 
   const renderOnceNavbar = (
     <Navbar
-    sidebarClicked={sidebarClicked}
-    setSidebarClicked={setSidebarClicked}
-    navbarClicked={navbarClicked}
-    setNavbarClicked={setNavbarClicked}/>
+      sidebarClicked={sidebarClicked}
+      setSidebarClicked={setSidebarClicked}
+      navbarClicked={navbarClicked}
+      setNavbarClicked={setNavbarClicked}
+    />
   );
 
   const renderOnceSidePanel = (
-    <SidePanel 
-    headerBoxRef={headerBoxRef} 
-    sidebarClicked={sidebarClicked}
-    setSidebarClicked={setSidebarClicked}
-    sidebarFixed={sidebarFixed}
-    setSidebarFixed={setSidebarFixed}
-     />
+    <SidePanel
+      headerBoxRef={headerBoxRef}
+      sidebarClicked={sidebarClicked}
+      setSidebarClicked={setSidebarClicked}
+      sidebarFixed={sidebarFixed}
+      setSidebarFixed={setSidebarFixed}
+    />
   );
 
   const renderOnceAudioPlayer = (
@@ -208,70 +206,77 @@ const Page = (props) => {
     />
   );
 
-
   return (
     <div className={styles["overarching"]}>
-      {props.headline?  (<div className={styles["primary-color-background"]}></div>): null}
-    <div className={styles["Page"]}>
-      <Header ref={headerBoxRef} className={styles["Header"]} />
-      {navFixed ? (
-        <div>
-          <div style={{ width: "100%", height: "66px" }}></div>
-          <div style={{ position: "fixed", width: "100%", top: "0px", zIndex: 1 }}>
-            {renderOnceNavbar}
-          </div>
-        </div>
-      ) : (
-        <div style={{ position: "relative", width: "100%" }}>
-          {renderOnceNavbar}
-        </div>
-      )}
-      {props.headline? props.headline : null}
-      <div className={styles["content-pane-side-bar"]}>
-      <div ref={contentPaneBoxRef} className={styles["content-pane"]}>
-        <div className={styles["main-pane"]}>
-          {props.mainPane}
-        </div>
-        <div className={styles["side-pane"]}></div>
-     
-      </div>
-      <div className={styles["side-bar"]}>
-      {sidebarFixed ? (
-          <div
-            style={{
-              position: "fixed",
-              top: topOffset,
-              right: "0px",
-              zIndex: 1,
-            }}
-          >
-            {renderOnceSidePanel}
+      {props.headline ? (
+        <div className={styles["primary-color-background"]}></div>
+      ) : null}
+      <div className={styles["Page"]}>
+        <Header ref={headerBoxRef} className={styles["Header"]} />
+        {navFixed ? (
+          <div>
+            <div style={{ width: "100%", height: "66px" }}></div>
+            <div
+              style={{
+                position: "fixed",
+                width: "100%",
+                top: "0px",
+                zIndex: 1,
+              }}
+            >
+              {renderOnceNavbar}
+            </div>
           </div>
         ) : (
-          <div style={{ marginTop: props.sidebarFixTopOffset}}>{renderOnceSidePanel}</div>
+          <div style={{ position: "relative", width: "100%" }}>
+            {renderOnceNavbar}
+          </div>
         )}
-      </div>
-      </div>
-      <div className={styles["footer-container"]}>
-      {audioPlayerFixed?(
-        <div>
-          <div
-            style={{
-              width: "100%"
-            }}
-          ></div>
-          <div style={{ position: "fixed", width: "100%", bottom: "0px" }}>
-            {renderOnceAudioPlayer}
+        {props.headline ? props.headline : null}
+        <div className={styles["content-pane-side-bar"]}>
+          <div ref={contentPaneBoxRef} className={styles["content-pane"]}>
+            <div className={styles["main-pane"]}>{props.mainPane}</div>
+            <div className={styles["side-pane"]}></div>
+          </div>
+          <div className={styles["side-bar"]}>
+            {sidebarFixed ? (
+              <div
+                style={{
+                  position: "fixed",
+                  top: topOffset,
+                  right: "0px",
+                  zIndex: 1,
+                }}
+              >
+                {renderOnceSidePanel}
+              </div>
+            ) : (
+              <div style={{ marginTop: sidebarFixTopOffset.current }}>
+                {renderOnceSidePanel}
+              </div>
+            )}
           </div>
         </div>
-      ) : (
-        <div style={{ position: "relative", width: "100%"}}>
-          {renderOnceAudioPlayer}
+        <div className={styles["footer-container"]}>
+          {audioPlayerFixed ? (
+            <div>
+              <div
+                style={{
+                  width: "100%",
+                }}
+              ></div>
+              <div style={{ position: "fixed", width: "100%", bottom: "0px" }}>
+                {renderOnceAudioPlayer}
+              </div>
+            </div>
+          ) : (
+            <div style={{ position: "relative", width: "100%" }}>
+              {renderOnceAudioPlayer}
+            </div>
+          )}
+          <Footer ref={footerBoxRef} />
         </div>
-      )}
-        <Footer ref={footerBoxRef}/>
       </div>
-    </div>
     </div>
   );
 };
