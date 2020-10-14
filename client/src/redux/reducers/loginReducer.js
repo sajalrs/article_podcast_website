c260fe220b88e936fa767d57bcde6490093abd23
sajalrs <sajalsatyal@gmail.com>
2020-10-14 14:54:18 -0500

    login reducer also stores user details

--- client/src/redux/reducers/loginReducer.js
+++ client/src/redux/reducers/loginReducer.js
@@ -1,10 +1,15 @@
-const loginReducer = (state={isLoggedIn: false}, action) => {
+const loginReducer = (state={isLoggedIn: false, user: null}, action) => {
     switch(action.type){
         case 'SET_IS_LOGGED_IN':
             return {
                 ...state,
                 isLoggedIn: action.payload
             };
+        case 'SET_USER':
+            return {
+                ...state, 
+                user: action.payload
+            }
         default: 
             return state;
     }
