dd238da6f8823e3376bfd93e09a14234846d55f1
sajalrs <sajalsatyal@gmail.com>
2020-10-15 15:09:19 -0500

    large card clone uses Card Component over its own declarations

---
+++ client/src/components/Cards/LargeCard/LargeCardClone.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./LargeCard.module.css";
+import {CardComponent} from "../Card"
+
+const LargeCard = (props) => {
+ 
+  return (
+    <CardComponent    image={props.image}
+    date={props.date}
+    title={props.title}
+    text={props.description}
+    author={props.author}
+    contentType={props.contentType}
+    link={props.link}
+    styles={styles}/>
+  );
+};
+
+
+export default LargeCard;
