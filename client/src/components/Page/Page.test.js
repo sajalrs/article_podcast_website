158834c71cf5d1b97e45f01163a2f0048970dedc
sajalrs <sajalsatyal@gmail.com>
2020-10-16 14:16:31 -0500

    added render with redux and router in testUtils and added pagetest

---
+++ client/src/components/Page/Page.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { screen } from "@testing-library/react";
+import Page from "./Page";
+import { renderWithReduxAndRouter, defaultStore } from "../../testUtils";
+
+global.scrollTo = jest.fn();
+
+describe("Page", () => {
+  test("Page Component Renders", () => {
+    renderWithReduxAndRouter(<Page />, {
+      initialState: defaultStore,
+    });
+
+    screen.debug();
+  });
+
+});
