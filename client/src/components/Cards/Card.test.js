7a473183382a8c5b8da12aefafc645b2d9d3755f
sajalrs <sajalsatyal@gmail.com>
2020-10-16 14:25:57 -0500

    card component test added

--- client/src/components/Cards/Card.test.js
+++ client/src/components/Cards/Card.test.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { screen } from "@testing-library/react";
-import Page from "./Page";
+import {CardComponent, Card} from "./Card";
 import { renderWithReduxAndRouter, defaultStore } from "../../testUtils";
 
 global.scrollTo = jest.fn();
 
-describe("Page", () => {
-  test("Page Component Renders", () => {
-    renderWithReduxAndRouter(<Page />, {
+describe("Card", () => {
+  test("Card Component Renders", () => {
+    renderWithReduxAndRouter(<CardComponent />, {
       initialState: defaultStore,
     });
+
+    screen.debug();
   });
 
 });
