179bad4d7f8017b15392f1e0ad15276f567c08e2
sajalrs <sajalsatyal@gmail.com>
2020-10-16 00:05:10 -0500

    todo mock url calls made by audio src

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -8,8 +8,29 @@ global.scrollTo = jest.fn();
 describe("App", () => {
   test("App Component Renders", () => {
     renderWithRedux(<App />, {
-      initialState: defaultStore
+      initialState: defaultStore,
     });
   });
 
+  // test("HTML5 Audio Renders", () => {
+  //   renderWithRedux(<App />, {
+  //     initialState: defaultStore,
+  //   });
+
+  //    expect(screen.getByTestId("html5-audio")).toBeInTheDocument();
+
+  // });
+
+  test("Audio Player Ref Set", () => {
+    const { getDispatchedActions } = renderWithRedux(<App />, {
+      initialState: defaultStore,
+    });
+    
+   
+    expect(
+      getDispatchedActions().filter((obj) => {
+        return obj.type === "SET_AUDIOPLAYER_REF";
+      }).payload
+    ).not.toBeNull();
+  });
 });
