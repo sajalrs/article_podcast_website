d0df4e68f75ef24b3e11d6a632fe6ffe949761ac
sajalrs <sajalsatyal@gmail.com>
2020-10-15 22:43:11 -0500

    render with Redux from testUtils functional. Able to get Dispatched Actions list

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,39 +1,29 @@
 import React from "react";
-import { render, screen} from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import App from "./App";
-import configureStore from 'redux-mock-store'
-import { Provider } from "react-redux";
+import { renderWithRedux, defaultStore } from "./testUtils";
 
 global.scrollTo = jest.fn();
-const mockStore = configureStore([]);
 
 describe("App", () => {
-  let store;
-  beforeEach(() => {
-    store = mockStore({
-
-    });
-  })
-
-  store.dispatch = jest.fn();
-
   test("App Component Renders", () => {
-    render(
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
+    const {
+      getDispatchedActions,
+    } = renderWithRedux(<App />, {
+      initialState: defaultStore
+    });
+
+    console.log(getDispatchedActions())
   });
 
-  test("Audio Player Ref Stored", () => {
-    render(
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
 
-    expect(store.dispatch).toHaveBeenCalledTimes(1);
-  });
+  // test("Audio Player Ref Stored", () => {
+  //   render(
+  //     <Provider store={store}>
+  //       <App />
+  //     </Provider>
+  //   );
 
- 
+  //   expect(store.dispatch).toHaveBeenCalledTimes(1);
+  // });
 });
