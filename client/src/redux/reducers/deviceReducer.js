937b73e61a589ef1117035050b06bb33f2025b14
sajalrs <sajalsatyal@gmail.com>
2020-10-28 17:42:20 -0500

    consolidated various screens into one screen property in device Reducer

--- client/src/redux/reducers/deviceReducer.js
+++ client/src/redux/reducers/deviceReducer.js
@@ -1,18 +1,17 @@
 const deviceReducer = (
   state = {
-    isMobile: window.innerWidth <= 550,
-    isTablet: window.innerWidth > 550 && window.innerWidth <= 1350,
-    isDesktop: window.innerWidth > 1350
+    screen:
+      window.innerWidth <= 550
+        ? "mobile"
+        : window.innerWidth > 550 && window.innerWidth <= 1350
+        ? "tablet"
+        : "desktop",
   },
   action
 ) => {
   switch (action.type) {
-    case "SET_IS_MOBILE":
-      return { ...state, isMobile: action.payload };
-    case "SET_IS_TABLET":
-      return { ...state, isTablet: action.payload };
-    case "SET_IS_DESKTOP":
-      return { ...state, isDesktop: action.payload };
+    case "SET_SCREEN":
+      return { ...state, screen: action.payload };
     default:
       return state;
   }
