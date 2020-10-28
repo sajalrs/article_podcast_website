c91d34f0a246bf7a558e909ca67e9bb4d3109f10
sajalrs <sajalsatyal@gmail.com>
2020-10-27 21:09:28 -0500

    added login prompt and changed time representation

--- client/src/components/Comment/CommentBar.js
+++ client/src/components/Comment/CommentBar.js
@@ -2,31 +2,55 @@ import React, { useState } from "react";
 import Comment from "./Comment";
 import TextEditor from "../TextEditor/TextEditor";
 import { Value } from "slate";
+import {useSelector} from "react-redux"
 import styles from "./Comment.module.css";
+
+const initialValue = {
+  document: {
+    nodes: [
+      {
+        object: "block",
+        type: "paragraph",
+        nodes: [
+          {
+            object: "text",
+            leaves: [
+              {
+                text: "Edit text and save to post comment...",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+};
+
+const loginWarning = {
+  document: {
+    nodes: [
+      {
+        object: "block",
+        type: "paragraph",
+        nodes: [
+          {
+            object: "text",
+            leaves: [
+              {
+                text: "Please login to post comments...",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+};
+
 
 
 const CommentBar = (props) => {
-  const initialValue = {
-    document: {
-      nodes: [
-        {
-          object: "block",
-          type: "paragraph",
-          nodes: [
-            {
-              object: "text",
-              leaves: [
-                {
-                  text: "Edit text and save to post comment...",
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-  };
-
+  const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
   const [textEditorValue, setTextEditorValue] = useState(
     Value.fromJSON(initialValue)
   );
@@ -39,15 +63,15 @@ const CommentBar = (props) => {
   return (
     <div>
       <h1 className={styles["heading-special"]}>Comments</h1>
-      <div
+    <div
         style={{
           borderStyle: "solid solid solid solid",
           borderColor: "var(--secondary-color)",
         }}
       >
         <TextEditor
-          value={textEditorValue}
-          setValue={setTextEditorValue}
+          value={isLoggedIn? textEditorValue : Value.fromJSON(loginWarning)}
+          setValue={isLoggedIn ? setTextEditorValue : () => {}}
           onSave={() => {
             postComment();
           }}
