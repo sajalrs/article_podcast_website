1341820858972d0a368d0f119b7c07e8f0cf9674
sajalrs <sajalsatyal@gmail.com>
2020-10-28 18:06:41 -0500

    added message validation method to validation.js

--- validation/validation.js
+++ validation/validation.js
@@ -1,25 +1,34 @@
 const Joi = require("@hapi/joi");
 
-
 const registerValidation = (data) => {
-    const schema = Joi.object({
-        name: Joi.string().min(6).required(),
-        email: Joi.string().min(6).required().email(),
-        password: Joi.string().min(6).required(),
-      });
-    
-    return schema.validate(data);
-}
+  const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
 
+  return schema.validate(data);
+};
 
 const loginValidation = (data) => {
-    const schema = Joi.object({
-        email: Joi.string().min(6).required().email(),
-        password: Joi.string().min(6).required(),
-      });
-    
-    return schema.validate(data);
-}
+  const schema = Joi.object({
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+  });
 
-module.exports.registerValidation = registerValidation
-module.exports.loginValidation = loginValidation
+  return schema.validate(data);
+};
+
+const messageValidation = (data) => {
+  const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    subject: Joi.string().min(6).required(),
+  });
+
+  return schema.validate(data);
+};
+
+module.exports.registerValidation = registerValidation;
+module.exports.loginValidation = loginValidation;
+module.exports.messageValidation = messageValidation;
