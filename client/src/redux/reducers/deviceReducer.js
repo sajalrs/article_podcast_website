2dca6c444ff17f97ece357730421e6fc760e7580
sajalrs <sajalsatyal@gmail.com>
2020-10-28 15:46:16 -0500

    added isTablet and isDesktop to device reducer

--- client/src/redux/reducers/deviceReducer.js
+++ client/src/redux/reducers/deviceReducer.js
@@ -1,7 +1,18 @@
-const deviceReducer = (state = { isMobile: window.innerWidth <= 550 }, action) => {
+const deviceReducer = (
+  state = {
+    isMobile: window.innerWidth <= 550,
+    isTablet: window.innerWidth > 550 && window.innerWidth <= 1350,
+    isDesktop: window.innerWidth > 1350
+  },
+  action
+) => {
   switch (action.type) {
     case "SET_IS_MOBILE":
       return { ...state, isMobile: action.payload };
+    case "SET_IS_TABLET":
+      return { ...state, isTablet: action.payload };
+    case "SET_IS_DESKTOP":
+      return { ...state, isDesktop: action.payload };
     default:
       return state;
   }
