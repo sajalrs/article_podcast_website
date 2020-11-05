8f9f3b0ac71b78d4d8336b0b2769128e0d7c1844
sajalrs <sajalsatyal@gmail.com>
2020-11-04 21:39:57 -0600

    only author and moderators able to edit

--- routes/articles.js
+++ routes/articles.js
@@ -64,20 +64,26 @@ router.post("/edit", verify, async (req, res) => {
     if (err) {
       res.send(err);
     } else {
-      article.title = req.body.title;
-      article.author = req.body.author;
-      article.date = req.body.date;
-      article.image = req.body.image;
-      article.content = req.body.content;
-      article.authorId = req.user._id;
-      article.isApproved = user && user.isModerator;
-      await article.save((error, data) => {
-        if (error) {
-          res.send(error);
-        } else {
-          res.json(data);
-        }
-      });
+      if(!user || (!user.isModerator && user._id !== article.authorId)){
+        res
+        .status(401)
+        .send({ error: "User not authorized to edit file" });
+      } else {
+        article.title = req.body.title;
+        article.author = req.body.author;
+        article.date = req.body.date;
+        article.image = req.body.image;
+        article.content = req.body.content;
+        article.isApproved = user.isModerator;
+        await article.save((error, data) => {
+          if (error) {
+            res.send(error);
+          } else {
+            res.json(data);
+          }
+        });
+      }
+    
     }
   });
 });
