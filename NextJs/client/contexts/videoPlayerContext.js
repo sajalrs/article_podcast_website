26748aafb3d0e2df6e0e1dfb2114ce10b60e05df
sajalrs <sajalsatyal@gmail.com>
2020-12-02 00:37:39 -0600

    video player context initialed through props

--- NextJs/client/contexts/videoPlayerContext.js
+++ NextJs/client/contexts/videoPlayerContext.js
@@ -2,23 +2,6 @@ import React, { useReducer, createContext } from "react";
 
 export const VideoPlayerContext = createContext();
 
-const initialState = {
-  selected:
-    "https://www.youtube.com/embed/vpWIvnnWxaY?rel=0&start=0&autoplay=1",
-  isPlaying: false,
-  youtubeVideos: [
-    {
-      id: "vpWIvnnWxaY",
-      title:
-        "The False 9 Podcast Live Premier League and Transfer Window Preview",
-      image: "https://img.youtube.com/vi/vpWIvnnWxaY/hqdefault.jpg",
-      link:
-        "https://www.youtube.com/embed/vpWIvnnWxaY?rel=0&start=0&autoplay=1",
-      date: "2020-09-11T16:56:50Z",
-    },
-  ],
-};
-
 const reducer = (state, action) => {
   switch (action.type) {
     case "SET_VIDEOPLAYER_SELECTED":
@@ -35,7 +18,7 @@ const reducer = (state, action) => {
 };
 
 export const VideoPlayerContextProvider = (props) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, props.initialState);
 
   return (
     <VideoPlayerContext.Provider value={[state, dispatch]}>
