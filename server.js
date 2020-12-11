dc76bb5c02aba96dca327ab87ff898026f01d6c7
sajalrs <sajalsatyal@gmail.com>
2020-12-11 13:58:24 -0600

    Should fix port problem on deployment

--- server.js
+++ server.js
@@ -4,32 +4,29 @@ const io = require("socket.io")(server);
 const next = require("next");
 const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
-
+require("dotenv/config");
 const nextHandler = nextApp.getRequestHandler();
-let port = 3000;
+let port = process.env.PORT || 3000;
 
 io.on("connect", (socket) => {
-    console.log("User Connected");
-    socket.on('join', data => {
-      socket.join(`${data._id}.${data.tokenCreated}`)
-      console.log("User joined room");
-    })
-  
-    socket.on('disconnect', ()=> {
-      socket.disconnect();
-      console.log("User Disconnected");
-    })
+  console.log("User Connected");
+  socket.on("join", (data) => {
+    socket.join(`${data._id}.${data.tokenCreated}`);
+    console.log("User joined room");
+  });
 
-
+  socket.on("disconnect", () => {
+    socket.disconnect();
+    console.log("User Disconnected");
+  });
 });
 
 nextApp.prepare().then(() => {
-  app.set('socket-io', io);  
+  app.set("socket-io", io);
   app.all("*", (req, res) => {
-    // req = {...req, socket: io};  
+    // req = {...req, socket: io};
     return nextHandler(req, res);
   });
-
 
   server.listen(port, (err) => {
     if (err) throw err;
