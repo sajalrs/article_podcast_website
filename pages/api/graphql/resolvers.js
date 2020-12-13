404d8b567d1319e6f639f94d788e5b350c6ecdc7
sajalrs <sajalsatyal@gmail.com>
2020-12-13 12:06:05 -0600

    added graphql resolver for podcasts

--- pages/api/graphql/resolvers.js
+++ pages/api/graphql/resolvers.js
@@ -12,22 +12,24 @@ try {
 export const resolvers = {
   Query: {
     youtubeLinks: () => YoutubeLink.find({}).sort("-date"),
-    podcasts: () => 
+    podcasts: () =>
       fetch(`https://anchor.fm/s/333e122c/podcast/rss`)
         .then((response) => response.text())
         .then((data) => {
-            let json = JSON.parse(parser.toJson(data, {reversible: false}));
-            return json.rss.channel.item.map((element) => {const text = element["description"].replace(/<[^>]+>/g, '').replace(/&nbsp;/, ''); return {
-              "title": element["title"],
-              "by": element["dc:creator"],
-              "link": element["enclosure"]["url"],
-              "date": element["pubDate"],
-              "image": element["itunes:image"]["href"],
-              "description": text.substring(0, text.indexOf("---"))
-            }
-            })
-            
-          })
-    
-},
+          let json = JSON.parse(parser.toJson(data, { reversible: false }));
+          return json.rss.channel.item.map((element) => {
+            const text = element["description"]
+              .replace(/<[^>]+>/g, "")
+              .replace(/&nbsp;/, "");
+            return {
+              title: element["title"],
+              by: element["dc:creator"],
+              link: element["enclosure"]["url"],
+              date: element["pubDate"],
+              image: element["itunes:image"]["href"],
+              description: text.substring(0, text.indexOf("---"))
+            };
+          });
+        }),
+  },
 };
