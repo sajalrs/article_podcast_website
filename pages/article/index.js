3a6c6452b3b1586ecc32b50b5be0917d71299533
sajalrs <sajalsatyal@gmail.com>
2020-12-16 12:56:49 -0600

    article page uses graphql over rest api

--- pages/article/index.js
+++ pages/article/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
 import LargeCard from "../../components/Cards/LargeCard/LargeCard.js";
-
 import Page from "../../components/Page/Page";
 import styles from "../../components/Page/Page.module.css";
 import Html from "slate-html-serializer";
@@ -11,45 +10,97 @@ import { LoginContext } from "../../contexts/reducers/loginContext";
 import { SocketContext } from "../../contexts/reducers/socketContext";
 import { Card } from "../../components/Cards/Card.js";
 import { useRouter } from "next/router";
+import { initializeApollo, addApolloState } from "../../lib/apolloClient";
+import { gql, useQuery, NetworkStatus } from "@apollo/client";
+
+export const ARTICLE_QUERY = gql`
+  query articleQuery($_id: String!) {
+    article(_id: $_id) {
+      _id
+      date
+      image
+      title
+      author
+      authorId
+      isApproved
+      content
+      comments {
+        _id
+        authorId
+        author
+        content
+        createdAt
+        updatedAt
+      }
+    }
+  }
+`;
+
 const ArticlePage = (props) => {
   const router = useRouter();
   const { id } = router.query;
+  const { loading, error, fetchMore, data, networkStatus, refetch } = useQuery(
+    ARTICLE_QUERY,
+    {
+      variables: { _id: id },
+      notifyOnNetworkStatusChange: true,
+    }
+  );
+
+  const loadingArticle = networkStatus === NetworkStatus.fetchMore;
+  const rules = getRules(styles);
+  const html = new Html({ rules });
+  let article;
+  if (loading && !loadingArticle) {
+    article = {};
+  } else {
+    article = {
+      ...data.article,
+      date: JSON.parse(data.article.date),
+      content: data.article.content? html.serialize(JSON.parse(data.article.content)) : `<p></p>`,
+      comments: data.article.comments.map((comment) => ({
+        ...comment,
+        content: JSON.parse(comment.content),
+        createdAt: JSON.parse(comment.createdAt),
+        updatedAt: JSON.parse(comment.updatedAt),
+      }))
+    };
+  }
+
   // const socket = useSelector((state) => state.network.socket);
   const [loginState, loginDispatch] = useContext(LoginContext);
   const [socketState, socketDispatch] = useContext(SocketContext);
   const socket = socketState.socket;
   const user = loginState.user;
-  const [article, setArticle] = useState(props.article);
+  // const [article, setArticle] = useState(props.article);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  useEffect(() => {
-    const getArticle = async () => {
-      const res = await fetch(
-        `/api/articles/page?id=${id}`
-      );
-      const json = await res.json();
-      const fetchedArticle = {
-        ...article,
-        comments: json.comments,
-      };
-      setArticle(fetchedArticle);
-    };
+  // useEffect(() => {
+  //   const getArticle = async () => {
+  //     const res = await fetch(`/api/articles/page?id=${id}`);
+  //     const json = await res.json();
+  //     const fetchedArticle = {
+  //       ...article,
+  //       comments: json.comments,
+  //     };
+  //     setArticle(fetchedArticle);
+  //   };
 
-    if (socket) {
-      socket.on("comments changed", (data) => {
-        console.log("Comments changed");
-        if (data.articleId === id) {
-          try {
-            getArticle();
-          } catch (error) {
-            console.log(error);
-          }
-        }
-      });
-    }
-  }, [socket]);
+  //   if (socket) {
+  //     socket.on("comments changed", (data) => {
+  //       console.log("Comments changed");
+  //       if (data.articleId === id) {
+  //         try {
+  //           getArticle();
+  //         } catch (error) {
+  //           console.log(error);
+  //         }
+  //       }
+  //     });
+  //   }
+  // }, [socket]);
 
   const postComment = (comment) => {
     const toPost = { id: id.toString(), content: comment };
@@ -118,28 +169,39 @@ const ArticlePage = (props) => {
 };
 
 export async function getServerSideProps({ query }) {
-  const rules = getRules(styles);
-  const html = new Html({ rules });
+  // const rules = getRules(styles);
+  // const html = new Html({ rules });
 
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_DOMAIN}/api/articles/page?id=${query.id}`
-  );
-  const json = await res.json();
-  const article = {
-    title: json.title,
-    author: json.author,
-    date: json.date,
-    image: json.image,
-    isApproved: json.isApproved,
-    content: json.content ? html.serialize(json.content) : `<p></p>`,
-    comments: json.comments,
-  };
+  const apolloClient = initializeApollo();
 
-  return {
-    props: {
-      article,
-    },
-  };
+  await apolloClient.query({
+    query: ARTICLE_QUERY,
+    variables: { _id: query.id },
+  });
+
+  return addApolloState(apolloClient, {
+    props: {},
+  });
+
+  // const res = await fetch(
+  //   `${process.env.NEXT_PUBLIC_DOMAIN}/api/articles/page?id=${query.id}`
+  // );
+  // const json = await res.json();
+  // const article = {
+  //   title: json.title,
+  //   author: json.author,
+  //   date: json.date,
+  //   image: json.image,
+  //   isApproved: json.isApproved,
+  //   content: json.content ? html.serialize(json.content) : `<p></p>`,
+  //   comments: json.comments,
+  // };
+
+  // return {
+  //   props: {
+  //     article,
+  //   },
+  // };
 }
 
 export default ArticlePage;
